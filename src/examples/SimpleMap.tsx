50ddfa6536cc4a1729133860cd1df632ce5e05d0
Sam Matenaer <smatenaer@esri.com>
2023-12-12 14:34:58 -0600

    update basics

--- src/examples/SimpleMap.tsx
+++ src/examples/SimpleMap.tsx
@@ -1,5 +1,5 @@
 import { ArcgisMap } from '@arcgis/map-components-react';
 
 export default function SimpleMap() {
-  return <ArcgisMap basemap="gray-vector" />;
+  return <ArcgisMap basemap="gray-vector" center="-90.3353,47.7496" zoom={15} />;
 }
--- src/examples/SimpleScene.tsx
+++ src/examples/SimpleScene.tsx
@@ -1,5 +1,5 @@
 import { ArcgisScene } from '@arcgis/map-components-react';
 
 export default function SimpleScene() {
-  return <ArcgisScene basemap="gray-vector" />;
+  return <ArcgisScene basemap="gray-vector" center="-90.3353,47.7496" zoom={2} />;
 }
